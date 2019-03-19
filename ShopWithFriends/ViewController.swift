20df8f6146e5731a63dd1412348fd90675be0624
John De Guzman <john.deguzman@symbilityintersect.com>
2019-03-19 11:24:56 -0400

    Initial Commit

---
+++ ShopWithFriends/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShopWithFriends
+//
+//  Created by John De Guzman on 2019-03-19.
+//  Copyright © 2019 John De Guzman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
